90c9f7fe02da5c387c04cb29d8e3dd5a6ffb3716
harinder786 <harinderss2899@gmail.com>
2021-03-23 00:34:57 +0530

    First commit

--- api/models.py
+++ api/models.py
@@ -14,7 +14,3 @@ class Task(models.Model):
     def __str__(self):
         return self.title
 
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
